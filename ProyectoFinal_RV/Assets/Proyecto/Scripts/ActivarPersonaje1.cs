391ea247a74c4ae9334cd1f16fbca2a79a5ac163
SantiagoRPRO <60673370+SantiagoRPRO@users.noreply.github.com>
2020-11-26 20:14:47 -0500

    ProyectoFinalRV
    version1

---
+++ ProyectoFinal_RV/Assets/Proyecto/Scripts/ActivarPersonaje1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarPersonaje1 : MonoBehaviour
+{
+    public GameObject p1;
+    void Awake()
+    {
+        p1.SetActive(false);
+    }
+    public void activarP1()
+    {
+        p1.SetActive(true);
+    }
+}
